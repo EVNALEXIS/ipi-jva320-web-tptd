9476f6617f25f824f4bdee98fa76dd6a16f8959a
Alexis EVENO <eveno.alexis@orange.fr>
2023-12-04 14:42:29 +0100

    Modification pour Q1: Page d'accueil

--- src/main/java/com/ipi/jva320/Jva320Application.java
+++ src/main/java/com/ipi/jva320/Jva320Application.java
@@ -1,5 +1,6 @@
 package com.ipi.jva320;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class Jva320Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Jva320Application.class, args);
+
     }
 
 }
---
+++ src/main/java/com/ipi/jva320/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.ipi.jva320.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(value = "/")
+    public String home(){
+        return "home";
+    }
+}
