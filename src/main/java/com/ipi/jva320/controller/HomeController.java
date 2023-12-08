2d93bed2c4b7e9c3ab2f73c5dccbce87276cce93
Alexis EVENO <eveno.alexis@orange.fr>
2023-12-08 11:16:45 +0100

    Il reste les questions Bonus

--- src/main/java/com/ipi/jva320/controller/HomeController.java
+++ src/main/java/com/ipi/jva320/controller/HomeController.java
@@ -1,14 +1,34 @@
 package com.ipi.jva320.controller;
 
+import com.ipi.jva320.model.SalarieAideADomicile;
+import com.ipi.jva320.service.SalarieAideADomicileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
-public class HomeController {
+@RequestMapping(path = "/")
 
-    @GetMapping(value = "/")
+public class HomeController {
+    @Autowired
+   private SalarieAideADomicileService salarieAideADomicileService ;
+
+    @GetMapping(value = "")
     public String home(){
         return "home";
     }
+
+    @ModelAttribute("nbSalarie")
+    public Long nbSalarie(){
+        return salarieAideADomicileService.countSalaries();
+    }
+
+
+
 }
--- src/main/java/com/ipi/jva320/service/DataInitService.java
+++ src/main/java/com/ipi/jva320/service/DataInitService.java
@@ -21,10 +21,12 @@ public class DataInitService implements CommandLineRunner {
         if (this.salarieAideADomicileService.countSalaries() != 0) {
             return;
         }
-
+        /*
         SalarieAideADomicile s1 = this.salarieAideADomicileService.creerSalarieAideADomicile(
                 new SalarieAideADomicile("Jean", LocalDate.parse("2022-12-05"), LocalDate.parse("2022-12-05"),
                         20, 0,
                         80, 10, 1));
+                        */
+
     }
 }
