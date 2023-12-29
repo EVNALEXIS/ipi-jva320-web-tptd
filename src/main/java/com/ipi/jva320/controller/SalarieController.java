8d63d3c65c849f2d57587c790fa5e3ec2dcbbd89
Alexis EVENO <eveno.alexis@orange.fr>
2023-12-29 13:19:37 +0100

    Version final (Pas fait les questions bonus)

--- src/main/java/com/ipi/jva320/controller/SalarieController.java
+++ src/main/java/com/ipi/jva320/controller/SalarieController.java
@@ -4,18 +4,11 @@ import com.ipi.jva320.exception.SalarieException;
 import com.ipi.jva320.model.SalarieAideADomicile;
 import com.ipi.jva320.service.SalarieAideADomicileService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Pageable;
-import java.time.LocalDate;
 import java.util.List;
-import java.util.stream.Stream;
 
 @Controller
 @RequestMapping(path = "/salaries")
@@ -29,19 +22,19 @@ public class SalarieController {
     @GetMapping(value = "/{id}")
     public String getSalarie(final ModelMap mp, @PathVariable Long id) {
         SalarieAideADomicile salarie = salarieAideADomicileService.getSalarie(id);
-        mp.put("salarie",salarie);
+        mp.put("salarie", salarie);
         return "detail_Salarie";
     }
 
     @GetMapping(value = "/aide/new")
-    public String afficherCreerSalarie(){
+    public String afficherCreerSalarie() {
         return "creer_salarie";
     }
 
     @PostMapping(value = "/save")
     public String creerSalarie(SalarieAideADomicile sad) throws SalarieException {
         salarieAideADomicileService.creerSalarieAideADomicile(sad);
-        return "redirect:/salaries/"+ sad.getId();
+        return "redirect:/salaries/" + sad.getId();
     }
 
     @PostMapping(value = "/modif/{id}")
@@ -72,24 +65,31 @@ public class SalarieController {
 
         return "redirect:/salaries";
     }
-    @GetMapping(value = "")
-    public String searchSalarie(@RequestParam(name = "nom", required = false) String nom, ModelMap mp){
 
-        List<SalarieAideADomicile> resultatRecherche = salarieAideADomicileService.getSalaries(nom);
-        mp.put("list_salarie",salarieAideADomicileService.getSalaries());
-        mp.put("resultatRecherche",resultatRecherche);
+    @GetMapping(params = "nom")
+    public String searchSalarie(@RequestParam(name = "nom", required = false) String nom, ModelMap mp) throws SalarieException {
+        List<SalarieAideADomicile> sad = salarieAideADomicileService.getSalaries(nom);
+        if (!sad.isEmpty()) {
+            mp.put("listSalarie", sad);
+            return "list";
 
+        } else {
+            throw new SalarieException("Le salarié recherché n'existe pas");
+        }
+
+    }
+
+    @GetMapping
+    public String list(ModelMap mp) {
+        mp.put("listSalarie", salarieAideADomicileService.getSalaries());
         return "list";
     }
 
+
     @ModelAttribute("nbSalarie")
-    public Long nbSalarie(){
+    public Long nbSalarie() {
         return salarieAideADomicileService.countSalaries();
     }
-
-
-
-
 
 
 }
--- src/main/java/com/ipi/jva320/exception/SalarieException.java
+++ src/main/java/com/ipi/jva320/exception/SalarieException.java
@@ -1,9 +1,15 @@
 package com.ipi.jva320.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Aucun salarié trouvé")
+
 public class SalarieException extends Exception {
 
     public SalarieException(String s) {
         super(s);
     }
+
 
 }
